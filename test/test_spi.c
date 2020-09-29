d7049d9baad21eb45340f911877c766a8b61c483
Kurt <stewart.kurt2@gmail.com>
2020-09-29 15:55:52 -0700

    getting lcov

--- test/test_spi.c
+++ test/test_spi.c
@@ -19,8 +19,7 @@ void test_spi_NeedToImplement(void)
 void test_CheckBaudRate(){
    uint16_t baud = 9600;
    uint16_t b2= 100;
-   //checkBaudRate_ExpectAndReturn(baud,1)
+   //checkBaudRate_ExpectAndReturn(baud,1);
    TEST_ASSERT_TRUE(checkBaudRate(baud));
-   
    TEST_ASSERT_FALSE(checkBaudRate(b2));
 }
